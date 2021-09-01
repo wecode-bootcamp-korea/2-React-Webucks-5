6ccc53ba1bf9757aff785edf5f69c0690d2b6e76
minjaekim <minjae2246@gmail.com>
2021-09-01 13:26:15 +0900

    Add: first commit 초기세팅 완료.

---
+++ pull_request_template.md
@@ -0,0 +1,35 @@
+# Mission 진행 상황 
+> 완료한 미션에 대해 체크해주세요.
+
+- [ ] Mission 0A 완료
+- [ ] Mission 0B 완료
+- [ ] Mission 0C 완료
+- [ ] Mission 1 완료
+- [ ] Mission 2 완료
+- [ ] Mission 3 완료
+- [ ] Mission 4 완료
+- [ ] Mission 5 완료
+- [ ] Mission 6 완료
+- [ ] Mission 7 완료
+- [ ] Mission 8 완료
+
+> 마크다운에서 체크를 하기 위해서는 [x] 이렇게 수정하시면 됩니다. [x ], [ x ] 가 아니라 정확하게 [x]여야 합니다. 혹은 마우스 클릭으로 체크해주세요.
+
+# Self-Refactoring 체크리스트
+> [Pre-Course 리팩토링 체크리스트](https://www.notion.so/wecode/Pre-Course-Refactoring-869f50bf6a934db98573229c600043ee)를 참고하여 리팩토링 진행해주세요.
+
+> [Foundation Fontend 리팩토링 체크리스트](https://www.notion.so/wecode/Foundation-Frontend-Refactoring-85e629d7a22841b2bd4917c9c72a1f58)를 참고하여 리팩토링을 진행한 항목에 대해 체크를 해주세요.
+
+- [ ] 0. Pre-Course 리팩토링 체크리스트 리팩토링 완료
+- [ ] 1. reset.scss & common.scss
+- [ ] 2. sass nesting 완료
+- [ ] 3. className 동적 활용
+- [ ] 4. destructuring
+- [ ] 5. Boolean 데이터 타입 활용
+- [ ] 6. 반복되는 코드 Array.map() 활용
+- [ ] 7. 계산된 속성명 활용
+- [ ] 8. \<a\>와 \<Link\> 구분하여 사용
+- [ ] 9. import 순서
+
+# Peer Reviews
+- [ ] Peer Reviews 받기 완료
---
+++ src/Routes.js
@@ -0,0 +1,69 @@
+import React ,{Component} from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+}
+from 'react-router-dom';
+
+// 김민재님 컴포넌트
+import LoginMinjaeKim from './pages/minjaekim/Login/Login';
+import ListMinjaeKim from './pages/minjaekim/List/List';
+import DetailMinjaeKim from './pages/minjaekim/Detail/Detail';
+
+// 이민재님 컴포넌트
+import LoginMinjaeLee from './pages/minjaelee/Login/Login';
+import ListMinjaeLee from './pages/minjaelee/List/List';
+import DetailMinjaeLee from './pages/minjaelee/Detail/Detail';
+
+
+// 이욱창님 컴포넌트
+import LoginWookChangLee from './pages/wookchanglee/Login/Login';
+import ListWookChangLee from './pages/wookchanglee/List/List';
+import DetailWookChangLee from './pages/wookchanglee/Detail/Detail';
+
+// 이성재님 컴포넌트
+import LoginSungJaeLee from './pages/sungjaelee/Login/Login';
+import ListSungJaeLee from './pages/sungjaelee/List/List';
+import DetailSungJaeLee from './pages/sungjaelee/Detail/Detail';
+
+// 김재원님 컴포넌트
+import LoginJaeWonKim from './pages/jaewonkim/Login/Login';
+import ListJaeWonKim from './pages/jaewonkim/List/List';
+import DetailJaeWonKim from './pages/jaewonkim/Detail/Detail';
+
+class Routes extends React.Component {
+  render(){
+    return(
+      <Router>
+        <Switch>
+            <Route exact path='/login-minjaekim' component={LoginMinjaeKim} />
+            <Route exact path='/list-minjaekim' component={ListMinjaeKim} />
+            <Route exact path='/detail-minjaekim' component={DetailMinjaeKim} />
+
+            <Route exact path='/login-minjaelee' component={LoginMinjaeLee} />
+            <Route exact path='/list-minjaelee' component={ListMinjaeLee} />
+            <Route exact path='/detail-minjaelee' component={DetailMinjaeLee} /> 
+
+            <Route exact path='/login-wookchanglee' component={LoginWookChangLee} />
+            <Route exact path='/list-wookchanglee' component={ListWookChangLee} />
+            <Route exact path='/detail-wookchanglee' component={DetailWookChangLee} /> 
+
+            <Route exact path='/login-sungjaelee' component={LoginSungJaeLee} />
+            <Route exact path='/list-sungjaelee' component={ListSungJaeLee} />
+            <Route exact path='/detail-sungjaelee' component={DetailSungJaeLee} /> 
+
+            <Route exact path='/login-jaewonkim' component={LoginJaeWonKim} />
+            <Route exact path='/list-jaewonkim' component={ListJaeWonKim} />
+            <Route exact path='/detail-jaewonkim' component={DetailJaeWonKim} /> 
+        </Switch>
+      </Router>
+    )
+  }
+}
+
+export default Routes;
+
+
+
+
