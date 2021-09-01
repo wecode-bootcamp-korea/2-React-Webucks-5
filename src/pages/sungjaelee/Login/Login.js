fda07b50da6d2a1bffaff2e5c33433bb6757c486
Hans Sung-jae Lee <sjhanslee@gmail.com>
2021-09-02 03:03:58 +0900

    Add: Mission 2 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/sungjaelee/Login/Login.js
+++ src/pages/sungjaelee/Login/Login.js
@@ -4,7 +4,30 @@ import { Link } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      idInput: null,
+      pwInput: null,
+    };
+    this.handleIdInput = this.handleIdInput.bind(this);
+    this.handlePwInput = this.handlePwInput.bind(this);
+  }
+
+  handleIdInput(e) {
+    this.setState({ idInput: e.target.value });
+    // console.log(e.target.value);
+  }
+
+  handlePwInput(e) {
+    this.setState({ pwInput: e.target.value });
+    // console.log(e.target.value);
+  }
+
   render() {
+    const { idInput, pwInput } = this.state;
+    console.log('idInput', idInput);
+    console.log('pwInput', pwInput);
     return (
       <div className="Login">
         <section className="box">
@@ -18,9 +41,15 @@ class Login extends Component {
               id="userId"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handleIdInput}
             />
             <div className="pwInputWrap">
-              <input id="userPw" type="password" placeholder="비밀번호" />
+              <input
+                id="userPw"
+                type="password"
+                placeholder="비밀번호"
+                onChange={this.handlePwInput}
+              />
               <span id="showHideText" className="invisible">
                 Show
               </span>
