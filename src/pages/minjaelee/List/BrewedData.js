e132714fae9f4dcac7171aec162ed8e045d0e5d5
thelmj <>
2021-09-02 04:24:46 +0900

    Add:Mission1 구현 완료

---
+++ src/pages/minjaelee/List/BrewedData.js
@@ -0,0 +1,14 @@
+const BREWED_DATA = [
+  {
+    id: 1,
+    name: '아이스 커피',
+    imgSrc: '/images/minjaelee/img13.jpg',
+  },
+  {
+    id: 2,
+    name: '오늘의 커피',
+    imgSrc: '/images/minjaelee/img14.jpg',
+  },
+];
+
+export default BREWED_DATA;
---
+++ src/pages/minjaelee/List/ColdbrewData.js
@@ -0,0 +1,64 @@
+const COLDBREW_DATA = [
+  {
+    id: 1,
+    name: '시그니처 더 블랙 콜드 브루',
+    imgSrc: '/images/minjaelee/img1.jpg',
+  },
+  {
+    id: 2,
+    name: '아이스크림 블렌딩 콜드 브루',
+    imgSrc: '/images/minjaelee/img2.jpg',
+  },
+  {
+    id: 3,
+    name: '나이트로 바닐라 크림',
+    imgSrc: '/images/minjaelee/img3.jpg',
+  },
+  {
+    id: 4,
+    name: '나이트로 콜드 브루',
+    imgSrc: '/images/minjaelee/img4.jpg',
+  },
+  {
+    id: 5,
+    name: '돌체 콜드 브루',
+    imgSrc: '/images/minjaelee/img5.jpg',
+  },
+  {
+    id: 6,
+    name: '바닐라 크림 콜드 브루',
+    imgSrc: '/images/minjaelee/img6.jpg',
+  },
+  {
+    id: 7,
+    name: '벨벳 다크 모카 나이트로',
+    imgSrc: '/images/minjaelee/img7.jpg',
+  },
+  {
+    id: 8,
+    name: '제주 비자림 콜드 브루',
+    imgSrc: '/images/minjaelee/img8.jpg',
+  },
+  {
+    id: 9,
+    name: '콜드 브루',
+    imgSrc: '/images/minjaelee/img9.jpg',
+  },
+  {
+    id: 10,
+    name: '콜드 브루 몰트',
+    imgSrc: '/images/minjaelee/img10.jpg',
+  },
+  {
+    id: 11,
+    name: '콜드 브루 오트 라떼',
+    imgSrc: '/images/minjaelee/img11.jpg',
+  },
+  {
+    id: 12,
+    name: '콜드 브루 플로트',
+    imgSrc: '/images/minjaelee/img12.jpg',
+  },
+];
+
+export default COLDBREW_DATA;
