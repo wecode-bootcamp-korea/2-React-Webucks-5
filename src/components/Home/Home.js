c8a1cf40803980f33841188de577fdff8c6186bd
wook95 <uclee95@gmail.com>
2021-09-01 14:59:01 +0900

    Add: eslint,prettier 설정 완료

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,27 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-class Home extends React.Component{
-  render(){
+class Home extends React.Component {
+  render() {
     return (
-    <div>
-
-
-      <Link to="/login-minjaekim"><button>김민재</button></Link>
-      <Link to="/login-jaewonkim"><button>김재원</button></Link>
-      <Link to="//login-minjaelee'"><button>이민재</button></Link>
-      <Link to="/login-sungjaelee"><button>이성재</button></Link>
-      <Link to="/login-wookchanglee"><button>이욱창</button></Link>
-  
-
-    </div>)
+      <div>
+        <Link to="/login-minjaekim">
+          <button>김민재</button>
+        </Link>
+        <Link to="/login-jaewonkim">
+          <button>김재원</button>
+        </Link>
+        <Link to="//login-minjaelee'">
+          <button>이민재</button>
+        </Link>
+        <Link to="/login-sungjaelee">
+          <button>이성재</button>
+        </Link>
+        <Link to="/login-wookchanglee">
+          <button>이욱창</button>
+        </Link>
+      </div>
+    );
   }
 }
 
