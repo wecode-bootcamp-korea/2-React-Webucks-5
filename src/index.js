a7ed2562a011c68a9ef5c37354fe215ee8f7cfe7
jambottle <jaewon.walks@gmail.com>
2021-09-01 16:26:48 +0900

    Modify: 코딩 컨벤션에 맞게 파일 수정 및 불필요 파일 삭제

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Routes';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
