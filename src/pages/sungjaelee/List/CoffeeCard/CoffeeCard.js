b765f13a9df7fa5bbce27e2623dbee311f1ce281
Hans Sung-jae Lee <sjhanslee@gmail.com>
2021-09-02 23:14:35 +0900

    Add: Mission 6 - 커피 리스트 페이지 커피별 좋아요 기능 구현

--- src/pages/sungjaelee/List/CoffeeCard/CoffeeCard.js
+++ src/pages/sungjaelee/List/CoffeeCard/CoffeeCard.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartO } from '@fortawesome/free-regular-svg-icons';
 
 import './CoffeeCard.scss';
 
 class CoffeeCard extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLiked: false,
+    };
+    this.handleLike = this.handleLike.bind(this);
+  }
   goToDetailPage = () => {
     console.log(this.props.history);
     this.props.history.push('/detail-sungjaelee');
     // this.props.history.goBack();
   };
 
+  handleLike() {
+    console.log('hello');
+    this.setState({ isLiked: !this.state.isLiked });
+  }
+
   render() {
+    const { isLiked } = this.state;
     const { name, img } = this.props;
 
     return (
@@ -26,7 +39,12 @@ class CoffeeCard extends Component {
         </div>
         <div className="nameContainer">
           <p className="name">{name}</p>
-          <FontAwesomeIcon icon={faHeartO} />
+          <FontAwesomeIcon
+            className={isLiked ? 'likeBtn fas' : 'likeBtn'}
+            icon={isLiked ? faHeart : faHeartO}
+            size="lg"
+            onClick={this.handleLike}
+          />
           {/* <i className="likeBtn far fa-heart"></i> */}
         </div>
       </li>
