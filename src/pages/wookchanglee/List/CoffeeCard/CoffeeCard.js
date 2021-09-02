10570fe088ebff5cd275b94cbc73d6c92fd3f7a0
wook95 <uclee95@gmail.com>
2021-09-02 14:57:59 +0900

    Add: Mission 6 완료 - 커피 리스트페이지 커피별 좋아요 기능 구현

--- src/pages/wookchanglee/List/CoffeeCard/CoffeeCard.js
+++ src/pages/wookchanglee/List/CoffeeCard/CoffeeCard.js
@@ -3,17 +3,28 @@ import { Link } from 'react-router-dom';
 import './coffeeCard.scss';
 
 class CoffeeCard extends React.Component {
+  state = { isHeartFilled: false };
+
   render() {
     return (
       <div className="CoffeeCard">
         <Link to="/detail-wookchanglee">
-          <div className="image-wraper">
+          <div className="wraper">
             <img src={this.props.img} alt={this.props.title} />
           </div>
         </Link>
-        <div className="image-title">
+        <div className="title">
           <p>{this.props.title}</p>
-          <i className="far fa-heart"></i>
+          <i
+            className={
+              this.state.isHeartFilled ? 'fas fa-heart' : 'far fa-heart'
+            }
+            onClick={() => {
+              this.setState({
+                isHeartFilled: !this.state.isHeartFilled,
+              });
+            }}
+          ></i>
         </div>
       </div>
     );
