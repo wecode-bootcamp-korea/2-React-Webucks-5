8745cd9ea9938f7ca4c4dfab5ea29960641d1f07
minjaekim <minjae2246@gmail.com>
2021-09-04 23:56:30 +0900

    Add : Mission 6 완료

--- src/pages/minjaekim/List/CoffeeCard/BrewedCoffeeCard.js
+++ src/pages/minjaekim/List/CoffeeCard/BrewedCoffeeCard.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react'
 import LIST_DATA from '../ListBrewedData'
 import { Link } from 'react-router-dom';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faHeart} from '@fortawesome/free-solid-svg-icons';
+import './listCoffeeCard.scss'
 
 class CoffeeWrapperImgAndName extends Component  {
+  constructor(props){
+    super(props);
+    this.state = {
+      heartColor: false,
+    }
+  }
+
+  isActive = ()=>{
+    this.setState({
+      heartColor: !this.state.heartColor
+    })
+  } 
+  
   render(){
+    const {heartColor} = this.state
     return (
       <div className="divWrapperAllImgAndName" key={this.props.key}>
         <Link to="/detail-minjaekim"className="divWrapperCoffeeImg">
           <img className="coffeeImg" src={this.props.src} alt={this.props.alt} />
         </Link>
-        <p className="coffeeName">{this.props.alt}</p>
+        <p className="coffeeName">{this.props.alt}
+        <FontAwesomeIcon icon={faHeart} className={heartColor? "heartIcon fillHeartIconColor" : "heartIcon"} onClick={this.isActive}/>
+        </p>
       </div>
       );
     }
--- src/pages/minjaekim/List/List.js
+++ src/pages/minjaekim/List/List.js
@@ -18,6 +18,8 @@ class ListPage extends React.Component {
                         ☕ 디카페인 에스프레소 샷 추가 기능 (일부 음료 제외)</h4>
                 </header>
                 <ColdBrewCoffeeCard />
+                {/* <CoffeeList data={coldbrewData} /> */ }
+                {/*  커피리스트 자체를 합쳐진 목업데이터에서 받아오면 같은 컴포넌트를 사용해도 되지 않을까? */}
             </div>
             <div className="mainCategoryWrapper brewedCoffeeSection">
                 <header className="categoryHeader brewedCoffeeSectionHead">
@@ -26,6 +28,7 @@ class ListPage extends React.Component {
                         ☕ 디카페인 에스프레소 샷 추가 기능 (일부 음료 제외)</h4>
                 </header>
                 <BrewedCoffeeCard />
+                {/* <CoffeList daata /> */}
             </div>
         </main>
       </div>  
