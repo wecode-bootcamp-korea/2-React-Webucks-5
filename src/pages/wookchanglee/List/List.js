387496e1fcbcce303499e1dcd45343a7d5fb865d
wook95 <uclee95@gmail.com>
2021-09-06 15:35:05 +0900

    Modify: mockData제거, fetch API로 데이터 통신 구현

--- src/pages/wookchanglee/List/List.js
+++ src/pages/wookchanglee/List/List.js
@@ -2,14 +2,13 @@ import React, { Component } from 'react';
 import Nav from '../../../components/Nav/Nav';
 import Footer from '../../../components/Footer/Footer';
 import CoffeeCardList from './CoffeeCardList/CoffeeCardList';
-import COFFEELIST_DATA from './listMockData';
 import './list.scss';
 
 class List extends Component {
   constructor() {
     super();
     this.state = {
-      COFFEELIST_DATA: COFFEELIST_DATA,
+      COFFEELIST_DATA: [],
       coffeeCardsName: {
         coldBrewCoffeeCards: '콜드 브루 커피',
         brewedCoffeeCards: '브루드 커피',
@@ -17,16 +16,32 @@ class List extends Component {
     };
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000/data/coffeeListData.json')
+      .then(res => res.json())
+      .then(res => {
+        this.setState({ COFFEELIST_DATA: res });
+      });
+  }
+
   render() {
     return (
       <div className="List">
         <Nav />
         <CoffeeCardList
-          COFFEELIST_DATA={this.state.COFFEELIST_DATA.coldBrewCoffeeCards}
+          COFFEELIST_DATA={
+            this.state.COFFEELIST_DATA.coldBrewCoffeeCards
+              ? this.state.COFFEELIST_DATA.coldBrewCoffeeCards
+              : []
+          }
           coffeeCardsName={this.state.coffeeCardsName.coldBrewCoffeeCards}
         />
         <CoffeeCardList
-          COFFEELIST_DATA={this.state.COFFEELIST_DATA.brewedCoffeeCards}
+          COFFEELIST_DATA={
+            this.state.COFFEELIST_DATA.brewedCoffeeCards
+              ? this.state.COFFEELIST_DATA.brewedCoffeeCards
+              : []
+          }
           coffeeCardsName={this.state.coffeeCardsName.brewedCoffeeCards}
         />
         <Footer />
