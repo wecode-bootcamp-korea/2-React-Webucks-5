1746d57c4643583385f12b0e193d2f0d235319c5
jambottle <jaewon.walks@gmail.com>
2021-09-09 01:50:47 +0900

    Refactor: Mock Data와 map 함수를 활용하여 Footer 컴포넌트 내에서 반복되는 코드의 중복 제거 (Footer.js)

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,68 +3,45 @@ import { Link } from 'react-router-dom';
 import './Footer.scss';
 
 class Footer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      myFooterLinks: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/data/myFooterLinks.json')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({
+          myFooterLinks: data.myFooterLinks,
+        });
+      });
+  }
+
   render() {
+    const { myFooterLinks } = this.state;
     return (
       <footer className="Footer">
-        <dl>
-          <dt className="footer-menu">
-            <Link to="#">COMPANY</Link>
-          </dt>
-          <dd className="footer-list">
-            <Link to="#">한눈에 보기</Link>
-            <Link to="#">스타벅스 사명</Link>
-            <Link to="#">스타벅스 소개</Link>
-            <Link to="#">국내 뉴스룸</Link>
-            <Link to="#">세계의 스타벅스</Link>
-            <Link to="#">글로벌 뉴스룸</Link>
-          </dd>
-        </dl>
-        <dl>
-          <dt className="footer-menu">
-            <Link to="#">CORPORATE SALES</Link>
-          </dt>
-          <dd className="footer-list">
-            <Link to="#">단체 및 기업 구매 안내</Link>
-          </dd>
-        </dl>
-        <dl>
-          <dt className="footer-menu">
-            <Link to="#">PARTNERSHIP</Link>
-          </dt>
-          <dd className="footer-list">
-            <Link to="#">신규 입점 제의</Link>
-            <Link to="#">협력 고객사 등록 신청</Link>
-          </dd>
-        </dl>
-        <dl>
-          <dt className="footer-menu">
-            <Link to="#">ONLINE COMMUNITY</Link>
-          </dt>
-          <dd className="footer-list">
-            <Link to="#">페이스북</Link>
-            <Link to="#">트위터</Link>
-            <Link to="#">유튜브</Link>
-            <Link to="#">블로그</Link>
-            <Link to="#">인스타그램</Link>
-          </dd>
-        </dl>
-        <dl>
-          <dt className="footer-menu">
-            <Link to="#">RECRUIT</Link>
-          </dt>
-          <dd className="footer-list">
-            <Link to="#">채용 소개</Link>
-            <Link to="#">채용 지원하기</Link>
-          </dd>
-        </dl>
-        <dl>
-          <dt className="footer-menu">
-            <Link to="#">WEBUCKS</Link>
-          </dt>
-          <dd className="footer-list">
-            <Link to="#"></Link>
-          </dd>
-        </dl>
+        {myFooterLinks.map(data => {
+          return (
+            <dl>
+              <dt className="footer-menu">
+                <Link to={data.titleLink}>{data.footerTitle}</Link>
+              </dt>
+              <dd className="footer-list">
+                {data.footerPages.map(data => {
+                  return (
+                    <Link key={data.id} to={data.pageLink}>
+                      {data.pageName}
+                    </Link>
+                  );
+                })}
+              </dd>
+            </dl>
+          );
+        })}
       </footer>
     );
   }
