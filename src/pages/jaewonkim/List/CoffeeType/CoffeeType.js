312e67d25de82c4f35ed0694f2502209c8af3e88
jambottle <jaewon.walks@gmail.com>
2021-09-08 09:20:33 +0900

    Modify: CSS 속성 순서 1차 변경 및 Sass Nesting 추가 적용

--- src/pages/jaewonkim/List/CoffeeType/CoffeeType.js
+++ src/pages/jaewonkim/List/CoffeeType/CoffeeType.js
@@ -4,14 +4,16 @@ import './CoffeeType.scss';
 class CoffeeType extends Component {
   render() {
     return (
-      <dt className="CoffeeType">
-        <h2>{this.props.title}</h2>
-        <img
-          alt="Decaf Coffee Icon"
-          src="https://image.istarbucks.co.kr/common/img/menu/logo_decaf.png"
-        />
-        <p>디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)</p>
-      </dt>
+      <dl className="CoffeeType">
+        <dt>
+          <h2>{this.props.title}</h2>
+          <img
+            alt="Decaf Coffee Icon"
+            src="https://image.istarbucks.co.kr/common/img/menu/logo_decaf.png"
+          />
+          <p>디카페인 에스프레소 샷 추가 가능 (일부 음료 제외)</p>
+        </dt>
+      </dl>
     );
   }
 }
